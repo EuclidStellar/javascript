68953abd89c68849585060d4c5fed92fa5019030
Gaurav Singh <100860877+EuclidStellar@users.noreply.github.com>
2023-09-27 00:04:36 +0530

    Update variables.js

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -3,7 +3,6 @@ let accountName = 'Gaurav Singh'
 var accountBalance = 1000000   // do not use var because javascript has an issue with var 
 accountCity = 'Bangalore'
 
-
 console.log("before operations");
 
 console.table([accountId, accountName, accountBalance, accountCity])
@@ -12,7 +11,5 @@ accountName = 'Euclid Stellar'
 accountBalance = 69
 accountCity = 'Lucknow'
 
-
 console.log("After operations");
-
 console.table([accountId, accountName, accountBalance, accountCity])
